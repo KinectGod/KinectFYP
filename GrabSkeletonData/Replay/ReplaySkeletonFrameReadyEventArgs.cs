dd0710b774ef125fff70d2a43042c95b6349766b
owen2801 <owen12888@yahoo.com.hk>
2013-10-29 20:39:57 +0800

    First attempt

---
+++ GrabSkeletonData/Replay/ReplaySkeletonFrameReadyEventArgs.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace GrabSkeletonData.Replay
+{
+    public class ReplaySkeletonFrameReadyEventArgs : EventArgs
+    {
+        public ReplaySkeletonFrame SkeletonFrame { get; set; } // construct a new value called 'SkeletonFrame', and gettable and settable
+    }
+}
