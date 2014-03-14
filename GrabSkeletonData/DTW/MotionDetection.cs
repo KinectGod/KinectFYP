7d32096cf461209eaf99762fc4d0819575fde023
owen2801 <owen12888@yahoo.com.hk>
2014-03-14 21:39:15 +0800

    fix all crashes

---
+++ GrabSkeletonData/DTW/MotionDetection.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace GrabSkeletonData.DTW
+{
+    class MotionDetection
+    {
+        public static void Detect(Point[] a1, Point[] a2, int dimension, double threshold, int [] _detection)
+        {
+            /*
+            DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Tick += new EventHandler (delegate(object s,EventArgs e){
+            Console.WriteLine("a1"+a1[1]);            
+            });
+            dispatcherTimer.Start();
+             * */
+            //int[] Detect = new int[a1.Length];
+            for (int i = 0; i < dimension; i++)
+            {
+                //a.x = xy-plane  a.y = yz-plane
+                if (Math.Abs(a1[i].X - a2[i].X) > threshold || Math.Abs(a1[i].Y - a2[i].Y) > threshold)
+                {
+
+                    if (i < 4 || i > 11)
+                        _detection[i] = textInstructionX(a1[i], a2[i]) + textInstructionYL(a1[i], a2[i]);   //left
+                    else
+                        _detection[i] = textInstructionX(a1[i], a2[i]) + textInstructionYR(a1[i], a2[i]);  //right
+                }
+                else
+                {
+                    _detection[i] = 0;
+                }
+            }
+
+            /// 
+
+            // return Detect;
+        }
+
+        /// <summary>
+        /// To generate the movement instrcution by interger.
+        /// The hundreds place and a.X represent ProjectToXZ;
+        /// 200=down ;300=up; 400=forward; 500=backward.
+        /// The tens place and a.Y represent ProjectToZY.
+        /// 20=right  40=left
+        /// (_LearnerAngle, _MasterAngle)
+        /// </summary>
+
+        /// <summary>
+        /// To generate the movement instrcution by interger.
+        /// The hundreds place and a.X represent ProjectToXZ;
+        /// 200=down ;300=up; 400=forward; 500=backward.
+        /// (_LearnerAngle, _MasterAngle)
+        /// </summary>
+        private static int textInstructionX(Point a1, Point a2)
+        {
+            if (a1.X >= 0 && a1.X < 90)
+            {
+                if (a2.X >= 0 && a2.X <= 90)
+                    if (a1.X - a2.X > 0)
+                        return 200;
+                    else
+                        return 300;
+                if (a2.X >= 90 && a2.X <= 270)
+                    return 400;
+                else
+                    return 300;
+            }
+            else if (a1.X >= 90 && a1.X < 180)
+            {
+                if (a2.X >= 90 && a2.X <= 180)
+                    if (a1.X - a2.X > 0)
+                        return 300;
+                    else
+                        return 200;
+                if (a2.X >= 180 && a2.X <= 270)
+                    return 300;
+                else
+                    return 500;
+            }
+            else if (a1.X >= 180 && a1.X < 270)
+            {
+                if (a2.X >= 180 && a2.X <= 270)
+                    if (a1.X - a2.X > 0)
+                        return 300;
+                    else
+                        return 200;
+                if (a2.X >= 90 && a2.X <= 180)
+                    return 200;
+                else
+                    return 500;
+            }
+            else
+            {
+                if (a2.X >= 270 && a2.X <= 360)
+                    if (a1.X - a2.X > 0)
+                        return 200;
+                    else
+                        return 300;
+                if (a2.X >= 0 && a2.X <= 90)
+                    return 200;
+                else
+                    return 400;
+            }
+        }
+
+
+        /// <summary>
+        /// To generate the movement instrcution by interger.
+        /// The tens place and a.Y represent ProjectToZY.  
+        /// 20 = right 40 = left
+        /// (_LearnerAngle, _MasterAngle)
+        /// </summary>
+        private static int textInstructionYL(Point a1, Point a2)
+        {
+            if (a1.X >= 0 && a1.X < 90)
+            {
+                if (a2.X >= 0 && a2.X <= 90)
+                    if (a1.X - a2.X > 0)
+                        return 20;
+                    else
+                        return 40;
+                else
+                    return 20;
+            }
+            else if (a1.X >= 90 && a1.X < 180)
+            {
+                if (a2.X >= 90 && a2.X <= 180)
+                    if (a1.X - a2.X > 0)
+                        return 20;
+                    else
+                        return 40;
+                if (a2.X >= 0 && a2.X <= 90)
+                    return 40;
+                else
+                    return 20;
+            }
+            else if (a1.X >= 180 && a1.X < 270)
+            {
+                if (a2.X >= 180 && a2.X <= 270)
+                    if (a1.X - a2.X > 0)
+                        return 20;
+                    else
+                        return 40;
+                if (a2.X >= 0 && a2.X <= 180)
+                    return 40;
+                else
+                    return 20;
+            }
+            else
+            {
+                if (a2.X >= 270 && a2.X <= 360)
+                    if (a1.X - a2.X > 0)
+                        return 20;
+                    else
+                        return 40;
+                else
+                    return 40;
+            }
+        }
+
+        /// <summary>
+        /// To generate the movement instrcution by interger.
+        /// The tens place and a.Y represent ProjectToZY.  
+        /// 20 = right 40 = left
+        /// (_LearnerAngle, _MasterAngle)
+        /// </summary>
+        private static int textInstructionYR(Point a1, Point a2)
+        {
+            if (a1.X >= 0 && a1.X < 90)
+            {
+                if (a2.X >= 0 && a2.X <= 90)
+                    if (a1.X - a2.X > 0)
+                        return 20;
+                    else
+                        return 40;
+                if (a2.X >= 90 && a2.X <= 180)
+                    return 20;
+                else
+                    return 40;
+            }
+            else if (a1.X >= 90 && a1.X < 180)
+            {
+                if (a2.X >= 90 && a2.X <= 180)
+                    if (a1.X - a2.X > 0)
+                        return 20;
+                    else
+                        return 40;
+                else
+                    return 40;
+            }
+            else if (a1.X >= 180 && a1.X < 270)
+            {
+                if (a2.X >= 180 && a2.X <= 270)
+                    if (a1.X - a2.X > 0)
+                        return 20;
+                    else
+                        return 40;
+                else
+                    return 20;
+            }
+            else
+            {
+                if (a2.X >= 270 && a2.X <= 360)
+                    if (a1.X - a2.X > 0)
+                        return 20;
+                    else
+                        return 40;
+                if (a2.X >= 0 && a2.X <= 180)
+                    return 20;
+                else
+                    return 40;
+            }
+        }
+    }
+}
--- GrabSkeletonData/MainWindow.xaml.cs
+++ GrabSkeletonData/MainWindow.xaml.cs
@@ -147,8 +147,8 @@
         private static KinectRecorder _recorder;
         private static KinectRecorder _colorrecorder;
 
-        private Stream recordstream;
-        private Stream recordcolorstream;
+        private static Stream _recordstream;
+        private static Stream _recordcolorstream;
 
         private KinectReplay _replay;
         private KinectReplay _colorreplay;
@@ -166,7 +166,7 @@
 
         private static Point[] _LearnerAngle;
 
-        private static int[] _detection = new int [dimension];
+        public static int[] detection = new int [dimension];
 
         /// <summary>
         /// Initializes a new instance of the MainWindow class
@@ -264,7 +264,7 @@
             {
                 var brush = new SolidColorBrush(Color.FromRgb(255, 0, 0));
                 int[] DetectionTemp = new int[dimension];
-                DetectionTemp = _detection;
+                DetectionTemp = detection;
                 string temp = "";
 
                 foreach (var data in skeletons)
@@ -597,27 +597,31 @@
 
             Stream recordStream = File.OpenRead(@path + "skeleton");
             _replay = new KinectReplay(recordStream);
+            recordStream.Close();
             _replay.SkeletonFrameReady += replay_SkeletonFrameReady;
             _replay.Start();
 
             Stream recordColorStream = File.OpenRead(@path + "colorStream");
             _colorreplay = new KinectReplay(recordColorStream);
+            recordColorStream.Close();
             _colorreplay.ColorImageFrameReady += replay_ColorImageFrameReady;
             _colorreplay.Start();
             _captureCountdownTimer.Dispose();
 
             status.Text = "Learning " + gestureList.Text;
 
+            /*
             // Clear the _video buffer and start from the beginning
             _video = new ArrayList();
-            path = ".\\Learnings\\" + gestureList.Text + "\\";
+            path = ".\\Learning\\" + gestureList.Text + "\\";
             if (!Directory.Exists(path))
             {
                 Directory.CreateDirectory(path);
             }
-            recordstream = File.Create(@path + "skeleton");
-            _recorder = new KinectRecorder(KinectRecordOptions.Skeletons, recordstream);
+            _recordstream = File.Create(@path + "skeleton");
+            _recorder = new KinectRecorder(KinectRecordOptions.Skeletons, _recordstream);
             //throw new NotImplementedException();
+             * */
         } 
 
         /// <summary>
@@ -653,10 +657,10 @@
             }
 
             _MasterMovesSaveFileLocation = path;
-            recordstream = File.Create(@path + "skeleton");
-            recordcolorstream = File.Create(@path + "colorStream");
-            _recorder = new KinectRecorder(KinectRecordOptions.Skeletons, recordstream);
-            _colorrecorder = new KinectRecorder(KinectRecordOptions.Color, recordcolorstream);
+            _recordstream = File.Create(@path + "skeleton");
+            _recordcolorstream = File.Create(@path + "colorStream");
+            _recorder = new KinectRecorder(KinectRecordOptions.Skeletons, _recordstream);
+            _colorrecorder = new KinectRecorder(KinectRecordOptions.Color, _recordcolorstream);
         }
        
 
@@ -687,8 +691,8 @@
 
             results.Text = gestureList.Text + " added";
             status.Text = "";
-            recordstream.Close();
-            recordcolorstream.Close();
+            _recordstream.Close();
+            _recordcolorstream.Close();
             // Scratch the _video buffer
             _video = new ArrayList();
 
@@ -706,11 +710,13 @@
 
             Stream recordStream = File.OpenRead(@path+"skeleton");
             _replay = new KinectReplay(recordStream);
+            recordStream.Close();
             _replay.SkeletonFrameReady += replay_SkeletonFrameReady;
             _replay.Start();
 
             Stream recordColorStream = File.OpenRead(@path + "colorStream");
             _colorreplay = new KinectReplay(recordColorStream);
+            recordColorStream.Close();
             _colorreplay.ColorImageFrameReady += replay_ColorImageFrameReady;
             _colorreplay.Start();
 
@@ -749,7 +755,7 @@
                     if (_LearnerAngle != null && _MasterAngle != null)
                     {
 
-                        MotionDetection(_LearnerAngle, _MasterAngle, 100);
+                        MotionDetection.Detect(_LearnerAngle, _MasterAngle, dimension, 100, detection);
                     }
                 }
             }
@@ -788,8 +794,8 @@
             _capturing = false;
             _replay.Stop();
             _colorreplay.Stop();
-            recordstream.Close();
-            recordcolorstream.Close();
+            _recordstream.Close();
+            _recordcolorstream.Close();
 
             MasterSkeletonCanvas.Children.Clear();
             LearnerSkeletonCanvas.Children.Clear();
@@ -840,214 +846,7 @@
 
         }
 
-        private void MotionDetection(Point[] a1, Point[] a2, double threshold)
-        {
-            /*
-            DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
-            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
-            dispatcherTimer.Tick += new EventHandler (delegate(object s,EventArgs e){
-            Console.WriteLine("a1"+a1[1]);            
-            });
-            dispatcherTimer.Start();
-             * */
-            //int[] Detect = new int[a1.Length];
-            for (int i = 0; i < dimension; i++)
-            {
-                //a.x = xy-plane  a.y = yz-plane
-                if (Math.Abs(a1[i].X - a2[i].X) > threshold || Math.Abs(a1[i].Y - a2[i].Y) > threshold)
-                {
-                    
-                    if (i < 4 || i > 11)
-                        _detection[i] = textInstructionX(a1[i], a2[i]) + textInstructionYL(a1[i], a2[i]);   //left
-                    else
-                       _detection[i] = textInstructionX(a1[i], a2[i]) + textInstructionYR(a1[i], a2[i]);  //right
-                }
-                else
-                {
-                    _detection[i] = 0;
-                }
-            }
-
-            /// 
-
-           // return Detect;
-        }
-
-        /// <summary>
-        /// To generate the movement instrcution by interger.
-        /// The hundreds place and a.X represent ProjectToXZ;
-        /// 200=down ;300=up; 400=forward; 500=backward.
-        /// The tens place and a.Y represent ProjectToZY.
-        /// 20=right  40=left
-        /// (_LearnerAngle, _MasterAngle)
-        /// </summary>
         
-        /// <summary>
-        /// To generate the movement instrcution by interger.
-        /// The hundreds place and a.X represent ProjectToXZ;
-        /// 200=down ;300=up; 400=forward; 500=backward.
-        /// (_LearnerAngle, _MasterAngle)
-        /// </summary>
-        private int textInstructionX(Point a1, Point a2)
-        {
-            if (a1.X >= 0 && a1.X < 90)
-            {
-                if (a2.X >= 0 && a2.X <= 90)
-                    if (a1.X - a2.X > 0)
-                        return 200;
-                    else
-                        return 300;
-                if (a2.X >= 90 && a2.X <= 270)
-                    return 400;
-                else
-                    return 300;
-            }
-            else if (a1.X >= 90 && a1.X < 180)
-            {
-                if (a2.X >= 90 && a2.X <= 180)
-                    if (a1.X - a2.X > 0)
-                        return 300;
-                    else
-                        return 200;
-                if (a2.X >= 180 && a2.X <= 270)
-                    return 300;
-                else
-                    return 500;
-            }
-            else if (a1.X >= 180 && a1.X < 270)
-            {
-                if (a2.X >= 180 && a2.X <= 270)
-                    if (a1.X - a2.X > 0)
-                        return 300;
-                    else
-                        return 200;
-                if (a2.X >= 90 && a2.X <= 180)
-                    return 200;
-                else
-                    return 500;
-            }
-            else
-            {
-                if (a2.X >= 270 && a2.X <= 360)
-                    if (a1.X - a2.X > 0)
-                        return 200;
-                    else
-                        return 300;
-                if (a2.X >= 0 && a2.X <= 90)
-                    return 200;
-                else
-                    return 400;
-            }
-        }
-
-
-        /// <summary>
-        /// To generate the movement instrcution by interger.
-        /// The tens place and a.Y represent ProjectToZY.  
-        /// 20 = right 40 = left
-        /// (_LearnerAngle, _MasterAngle)
-        /// </summary>
-        private int textInstructionYL(Point a1, Point a2)
-        {
-            if (a1.X >= 0 && a1.X < 90)
-            {
-                if (a2.X >= 0 && a2.X <= 90)
-                    if (a1.X - a2.X > 0)
-                        return 20;
-                    else
-                        return 40;
-                else
-                    return 20;
-            }
-            else if (a1.X >= 90 && a1.X < 180)
-            {
-                if (a2.X >= 90 && a2.X <= 180)
-                    if (a1.X - a2.X > 0)
-                        return 20;
-                    else
-                        return 40;
-                if (a2.X >= 0 && a2.X <= 90)
-                    return 40;
-                else
-                    return 20;
-            }
-            else if (a1.X >= 180 && a1.X < 270)
-            {
-                if (a2.X >= 180 && a2.X <= 270)
-                    if (a1.X - a2.X > 0)
-                        return 20;
-                    else
-                        return 40;
-                if (a2.X >= 0 && a2.X <= 180)
-                    return 40;
-                else
-                    return 20;
-            }
-            else
-            {
-                if (a2.X >= 270 && a2.X <= 360)
-                    if (a1.X - a2.X > 0)
-                        return 20;
-                    else
-                        return 40;
-                else
-                    return 40;
-            }
-        }
-
-        /// <summary>
-        /// To generate the movement instrcution by interger.
-        /// The tens place and a.Y represent ProjectToZY.  
-        /// 20 = right 40 = left
-        /// (_LearnerAngle, _MasterAngle)
-        /// </summary>
-        private int textInstructionYR(Point a1, Point a2)
-        {
-            if (a1.X >= 0 && a1.X < 90)
-            {
-                if (a2.X >= 0 && a2.X <= 90)
-                    if (a1.X - a2.X > 0)
-                        return 20;
-                    else
-                        return 40;
-                if (a2.X >= 90 && a2.X <= 180)
-                    return 20;
-                else
-                    return 40;
-            }
-            else if (a1.X >= 90 && a1.X < 180)
-            {
-                if (a2.X >= 90 && a2.X <= 180)
-                    if (a1.X - a2.X > 0)
-                        return 20;
-                    else
-                        return 40;
-                else
-                    return 40;
-            }
-            else if (a1.X >= 180 && a1.X < 270)
-            {
-                if (a2.X >= 180 && a2.X <= 270)
-                    if (a1.X - a2.X > 0)
-                        return 20;
-                    else
-                        return 40;
-                else
-                    return 20;
-            }
-            else
-            {
-                if (a2.X >= 270 && a2.X <= 360)
-                    if (a1.X - a2.X > 0)
-                        return 20;
-                    else
-                        return 40;
-                if (a2.X >= 0 && a2.X <= 180)
-                    return 20;
-                else
-                    return 40;
-            }
-        }
 
        
 
